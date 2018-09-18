70e868083913a840cde2e04812dfb2dc9460cd58
Vadivel <vmvadivel@gmail.com>
2018-09-18 16:08:54 +0530

    Add project files.

---
+++ LetsTalkSignalR/ChatHub.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using LetsTalkSignalR.Models;
+using Microsoft.AspNetCore.SignalR;
+
+namespace LetsTalkSignalR
+{
+    public class ChatHub : Hub
+    {
+        public async Task SendMessage(string name, string text)
+        {
+            var message = new ChatMessage
+            {
+                SenderName = name,
+                Text = text,
+                SentAt = DateTimeOffset.UtcNow
+            };
+
+            //Broadcast to all clients
+            await Clients.All.SendAsync(
+                "ReceiveMessage",
+                message.SenderName,
+                message.Text,
+                message.SentAt
+                );
+        }
+    }
+}
---
+++ LetsTalkSignalR/Models/ChatMessage.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LetsTalkSignalR.Models
+{
+    public class ChatMessage
+    {
+        public string SenderName { get; set; }
+        public string Text { get; set; }
+        public DateTimeOffset SentAt { get; set; }
+    }
+}
